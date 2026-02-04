b46db67fde4b58ba0542cf4fbdae71f8a919cbf8
olhkyle <kylekwon.dev@gmail.com>
2026-02-04 16:23:41 +0900

    ui : update not-found ui

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link';
 import { route } from '@/constants';
+import { Button } from '@/components';
 
 export default async function NotFound() {
 	return (
-		<div className="flex-1 ui-flex-center h-screen">
-			<Link href={route.SERVICE.ROOT} className="p-3 bg-black text-white rounded-lg">
-				Go To Home
-			</Link>
+		<div className="flex-1 ui-flex-center min-h-screen">
+			<div className="ui-flex-center gap-12 flex-col">
+				<h1 className="p-4 text-black text-4xl font-black rounded-lg">Skevv</h1>
+				<p className="font-black text-2xl font-mono">404</p>
+				<Button asChild variant="secondary">
+					<Link href={route.SERVICE.ROOT}>홈으로 가기</Link>
+				</Button>
+			</div>
 		</div>
 	);
 }
